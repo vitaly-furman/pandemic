91a55a672f0cadaa019147af31826d36697947ef
vitaly-furman <furman4488@gmail.com>
2023-04-25 17:16:35 +0300

    added swagger ui

--- src/router.ts
+++ src/router.ts
@@ -2,7 +2,12 @@ import { Router } from 'express';
 import recordRouter from './routes/record.router';
 import notificationRouter from './routes/notification.router';
 
+import swaggerUi from 'swagger-ui-express';
+import * as swaggerDocument from './swagger.json';
+
 const appRouter = Router();
+
+appRouter.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 appRouter.use('/api/v1/record', recordRouter);
 appRouter.use('/api/v1/notify', notificationRouter);
